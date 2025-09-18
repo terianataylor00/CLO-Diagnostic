e171df994220b47df25d3686cfb3c66f83a895a8
terianataylor00 <terianataylor00@gmail.com>
2025-09-18 12:47:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Show Button Selection
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".selection-btn");
 
@@ -7,15 +8,77 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const group = button.dataset.group;
 
-      // Remove active from all buttons in the same group
+      // Remove active coloring from all buttons in the same group
       buttons.forEach((btn) => {
         if (btn.dataset.group === group) {
           btn.classList.remove("active");
         }
       });
 
-      // Add active to the clicked button
+      // Add coloring to the clicked button
       button.classList.add("active");
     });
   });
 });
+
+
+//Start Button Navigation
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('startBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./cQuestions.html')
+  })
+})
+
+//Nav Buttons cQuestions
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('c-backBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./index.html')
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('c-nextBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./xQuestions.html')
+  })
+})
+
+//Nav Buttons xQuestions
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('x-backBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./cQuestions.html')
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('x-nextBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./oQuestions.html')
+  })
+})
+
+//Nav Buttons oQuestions
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('o-backBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./xQuestions.html')
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('o-nextBtn');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    window.location.assign('./results.html')
+  })
+})
+
